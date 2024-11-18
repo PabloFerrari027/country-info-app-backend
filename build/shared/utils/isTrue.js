1eb30680138a1e17193323cf6b567b8a3105d726
Pablo Ferrari <pabloferraricaliari@gmail.com>
2024-11-18 09:37:03 -0300

    ...

--- build/shared/utils/isTrue.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.isTrue = void 0;
-class isTrue {
-    static execute(input) {
-        return !!input.filter(item => !!item);
-    }
-}
-exports.isTrue = isTrue;
